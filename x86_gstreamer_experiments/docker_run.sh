689d2e8c3f5ae31c017273bbf3a849643e23c7c5
Alex Nazarov <artint@ArtintPortableBook>
2023-04-27 23:13:23 +0300

    Update to use docker to build opencv with gstreamer support

--- x86_gstreamer_experiments/docker_run.sh
+++ x86_gstreamer_experiments/docker_run.sh
@@ -1,2 +1,2 @@
-docker build -t gstreamer_experiments - < Dockerfile
-docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/playground  gstreamer_experiments
+docker build -t gstreamer_experiments:20.04 - < Dockerfile
+docker run --rm -it --user $(id -u):$(id -g) -v /home/artint/Projects/MachineLearning/Otus2022/Project/opencv-python:/playground  gstreamer_experiments:20.04
