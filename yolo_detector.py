3f9fe8e30ed0fe87c5c2afb050635c09d8f027a9
Alex Nazarov <artint@ArtintPortableBook>
2023-06-06 09:25:38 +0300

    Main class components implemented

--- yolo_detector.py
+++ yolo_detector.py
@@ -1,8 +1,6 @@
-import collections
 #TODO - replace to some kind of async
 import threading
-
-
+import queue
 
 class YoloDetector:
     def __init__(self, capturer, img_transformer, inferer, postprocessor, visualizer, result_storer):
@@ -14,16 +12,19 @@ class YoloDetector:
         self._result_storer = result_storer
 
         self._preprocessing_thread = threading.Thread(None, self._preprocess_tf)
-        # self._preprocessing_thread.start()
-
         self._postprocessing_thread = threading.Thread(None, self._postprocess_tf)
-        # self._postprocessing_thread.start()
 
         self._global_stop = False
 
         #magic numbers approximately correspond to smth 
-        self._infer_queue = collections.deque(maxlen=20)
-        self._result_queue = collections.deque(maxlen=10)
+        # self._infer_queue = collections.deque(maxlen=20)
+        # self._result_queue = collections.deque(maxlen=10)
+        self._infer_queue = queue.Queue(maxsize=20)
+        self._result_queue = queue.Queue(maxsize=10)
+
+    def __del__(self):
+        self._preprocessing_thread.join()
+        self._postprocessing_thread.join()
 
     def _preprocess_tf(self):
         while True:
@@ -34,17 +35,25 @@ class YoloDetector:
             
             orig_img, resized_imd = frames
             ready_for_infer_img = self._img_transformer.transform(resized_imd)
-            self._infer_queue.append(orig_img, ready_for_infer_img)
+            self._infer_queue.put(orig_img, ready_for_infer_img)
 
 
     def _postprocess_tf(self):
-        while not self._global_stop:
-            orig_frame, raw_infer_results = self._result_queue.pop()
+        while not self._global_stop and not self._result_queue.empty():
+            orig_frame, raw_infer_results = self._result_queue.get()
             objects = self._postprocessor.process_raw_data(raw_infer_results)
             resulting_frame = self._visualizer.draw_objects(orig_frame, objects)
 
             #TODO - estimate probably move to other storage
             self._result_storer.store(resulting_frame)
-            
+
     def _main_fn(self):
-        pass
+        self._preprocessing_thread.start()
+        self._postprocessing_thread.start()
+
+        while not self._global_stop and not self._infer_queue.empty():
+            while not self._infer_queue.empty():
+                orig_img, ready_for_infer_img = self._infer_queue.get()
+
+            raw_infer_results = self._inferer.infer(ready_for_infer_img)
+            self._result_queue.put(orig_img, raw_infer_results)
