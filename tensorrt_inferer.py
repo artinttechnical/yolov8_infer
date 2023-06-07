74a2df0f85f636a4f1b524492ca62f684211afc9
Alex Nazarov <artint@ArtintPortableBook>
2023-06-07 20:01:32 +0300

    Tensorrt inferer implement

--- tensorrt_inferer.py
+++ tensorrt_inferer.py
@@ -1,11 +1,3 @@
-"""yolo_with_plugins.py
-
-Implementation of TrtYOLO class with the yolo_layer plugins.
-"""
-
-
-from __future__ import print_function
-
 import numpy as np
 import tensorrt as trt
 import pycuda.driver as cuda
@@ -29,119 +21,91 @@ class HostDeviceMem(object):
         del self.device
         del self.host        
 
+class TensorrtInferer:
 
-def get_input_shape(engine):
-    """Get input shape of the TensorRT YOLO engine."""
-    binding = engine[0]
-    assert engine.binding_is_input(binding)
-    binding_dims = engine.get_binding_shape(binding)
-    if len(binding_dims) == 4:
-        return tuple(binding_dims[2:])
-    elif len(binding_dims) == 3:
-        return tuple(binding_dims[1:])
-    else:
-        raise ValueError('bad dims of binding %s: %s' % (binding, str(binding_dims)))
-
-
-def allocate_buffers(engine):
-    """Allocates all host/device in/out buffers required for an engine."""
-    inputs = []
-    outputs = []
-    bindings = []
-    output_idx = 0
-    stream = cuda.Stream()
-    for binding in engine:
-        binding_dims = engine.get_binding_shape(binding)
+    def _get_input_shape(self):
+        """Get input shape of the TensorRT YOLO engine."""
+        binding = self._engine[0]
+        assert self._engine.binding_is_input(binding)
+        binding_dims = self._engine.get_binding_shape(binding)
         if len(binding_dims) == 4:
-            # explicit batch case (TensorRT 7+)
-            size = trt.volume(binding_dims)
+            return tuple(binding_dims[2:])
         elif len(binding_dims) == 3:
-            # implicit batch case (TensorRT 6 or older)
-            size = trt.volume(binding_dims) * engine.max_batch_size
+            return tuple(binding_dims[1:])
         else:
             raise ValueError('bad dims of binding %s: %s' % (binding, str(binding_dims)))
-        dtype = trt.nptype(engine.get_binding_dtype(binding))
-        # Allocate host and device buffers
-        host_mem = cuda.pagelocked_empty(size, dtype)
-        device_mem = cuda.mem_alloc(host_mem.nbytes)
-        # Append the device buffer to device bindings.
-        bindings.append(int(device_mem))
-        # Append to the appropriate list.
-        if engine.binding_is_input(binding):
-            inputs.append(HostDeviceMem(host_mem, device_mem))
-        else:
-            # each grid has 3 anchors, each anchor generates a detection
-            # output of 7 float32 values
-            assert size % 7 == 0
-            outputs.append(HostDeviceMem(host_mem, device_mem))
-            output_idx += 1
-    assert len(inputs) == 1
-    assert len(outputs) == 1
-    return inputs, outputs, bindings, stream
 
 
-def do_inference(context, bindings, inputs, outputs, stream, batch_size=1):
-    """do_inference (for TensorRT 6.x or lower)
+    def _allocate_buffers(self):
+        """Allocates all host/device in/out buffers required for an engine."""
+        inputs = []
+        outputs = []
+        bindings = []
+        output_idx = 0
+        stream = cuda.Stream()
+        for binding in self._engine:
+            binding_dims = self._engine.get_binding_shape(binding)
+            if len(binding_dims) == 4:
+                # explicit batch case (TensorRT 7+)
+                size = trt.volume(binding_dims)
+            elif len(binding_dims) == 3:
+                # implicit batch case (TensorRT 6 or older)
+                size = trt.volume(binding_dims) * self._engine.max_batch_size
+            else:
+                raise ValueError('bad dims of binding %s: %s' % (binding, str(binding_dims)))
+            dtype = trt.nptype(self._engine.get_binding_dtype(binding))
+            # Allocate host and device buffers
+            host_mem = cuda.pagelocked_empty(size, dtype)
+            device_mem = cuda.mem_alloc(host_mem.nbytes)
+            # Append the device buffer to device bindings.
+            bindings.append(int(device_mem))
+            # Append to the appropriate list.
+            if self._engine.binding_is_input(binding):
+                inputs.append(HostDeviceMem(host_mem, device_mem))
+            else:
+                # each grid has 3 anchors, each anchor generates a detection
+                # output of 7 float32 values
+                assert size % 7 == 0
+                outputs.append(HostDeviceMem(host_mem, device_mem))
+                output_idx += 1
+        assert len(inputs) == 1
+        assert len(outputs) == 1
+        return inputs, outputs, bindings, stream
 
-    This function is generalized for multiple inputs/outputs.
-    Inputs and outputs are expected to be lists of HostDeviceMem objects.
-    """
-    # Transfer input data to the GPU.
-    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
-    # Run inference.
-    context.execute_async(batch_size=batch_size,
-                          bindings=bindings,
-                          stream_handle=stream.handle)
-    # Transfer predictions back from the GPU.
-    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
-    # Synchronize the stream
-    stream.synchronize()
-    # Return only the host outputs.
-    return [out.host for out in outputs]
+    def _do_inference_v2(self):
+        """do_inference_v2 (for TensorRT 7.0+)
 
+        This function is generalized for multiple inputs/outputs for full
+        dimension networks.
+        Inputs and outputs are expected to be lists of HostDeviceMem objects.
+        """
+        # Transfer input data to the GPU.
+        [cuda.memcpy_htod_async(inp.device, inp.host, self.stream) for inp in self.inputs]
+        # Run inference.
+        self.context.execute_async_v2(bindings=self.bindings, stream_handle=self.stream.handle)
+        # Transfer predictions back from the GPU.
+        [cuda.memcpy_dtoh_async(out.host, out.device, self.stream) for out in self.outputs]
+        # Synchronize the stream
+        self.stream.synchronize()
+        # Return only the host outputs.
+        return [out.host for out in self.outputs]
 
-def do_inference_v2(context, bindings, inputs, outputs, stream):
-    """do_inference_v2 (for TensorRT 7.0+)
-
-    This function is generalized for multiple inputs/outputs for full
-    dimension networks.
-    Inputs and outputs are expected to be lists of HostDeviceMem objects.
-    """
-    # Transfer input data to the GPU.
-    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
-    # Run inference.
-    context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
-    # Transfer predictions back from the GPU.
-    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
-    # Synchronize the stream
-    stream.synchronize()
-    # Return only the host outputs.
-    return [out.host for out in outputs]
-
-
-class TrtYOLO(object):
-    """TrtYOLO class encapsulates things needed to run TRT YOLO."""
-
-    def _load_engine(self):
-        TRTbin = 'yolo/%s.trt' % self.model
-        with open(TRTbin, 'rb') as f, trt.Runtime(self.trt_logger) as runtime:
+    def _load_engine(self, engine_path: Path):
+        with open(engine_path, 'rb') as f, trt.Runtime(self.trt_logger) as runtime:
             return runtime.deserialize_cuda_engine(f.read())
 
     def __init__(self, engine_path: Path):
-        """Initialize TensorRT plugins, engine and conetxt."""
         self.cuda_ctx = None
 
-        self.inference_fn = do_inference if trt.__version__[0] < '7' \
-                                         else do_inference_v2
         self.trt_logger = trt.Logger(trt.Logger.INFO)
         self.engine = self._load_engine()
 
-        self.input_shape = get_input_shape(self.engine)
+        self.input_shape = self._get_input_shape(self.engine)
 
         try:
             self.context = self.engine.create_execution_context()
             self.inputs, self.outputs, self.bindings, self.stream = \
-                allocate_buffers(self.engine)
+                self._allocate_buffers(self.engine)
         except Exception as e:
             raise RuntimeError('fail to allocate CUDA resources') from e
         finally:
@@ -154,30 +118,14 @@ class TrtYOLO(object):
         del self.inputs
         del self.stream
 
-    def detect(self):
+    def detect(self, infer_data: np.array):
         # Set host input to the image. The do_inference() function
         # will copy the input to the GPU before executing.
-        self.infer_fps += 1
-        active_len = self.input_queue.qsize()
-        for _ in range(active_len - 1):
-            self.input_queue.get()
-
-        if active_len == 0 and self.stop:
-            print("Quit detect function")
-            return
-
-        img, img_resized = self.input_queue.get()
-        self.inputs[0].host = np.ascontiguousarray(img_resized)
+        self.inputs[0].host = np.ascontiguousarray(infer_data)
         if self.cuda_ctx:
             self.cuda_ctx.push()
-        trt_outputs = self.inference_fn(
-            context=self.context,
-            bindings=self.bindings,
-            inputs=self.inputs,
-            outputs=self.outputs,
-            stream=self.stream)
+        trt_outputs = self._do_inference_v2()
         if self.cuda_ctx:
             self.cuda_ctx.pop()
 
-        self.infer_fps += 1
-        self.inference_queue.put((img, trt_outputs[0].copy()))
+        return trt_outputs[0].copy()
