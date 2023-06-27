989e681a71ab22ce5794297e49c74ff52f6516c5
Alex Nazarov <artint@ArtintPortableBook>
2023-06-27 09:17:24 +0300

    Refactor in genereal

--- yolo_postprocessor.py
+++ yolo_postprocessor.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2    
 
+OBJECT_COORDINATES_NUM = 4 #x, y, w, h
+
 def _nms_boxes(self, detections, nms_threshold):
     """Apply the Non-Maximum Suppression (NMS) algorithm on the bounding
     boxes with their confidence scores and return an array with the
@@ -43,22 +45,25 @@ def _nms_boxes(self, detections, nms_threshold):
 
 
 class YoloPostprocessor:
-    def __init__(self, orig_image_shape, categories_num, object_probability, nms_threshold):
-        self._orig_image_shape = orig_image_shape
+    def __init__(self, axis_scaling, categories_num, object_probability, nms_threshold):
+        self._axis_scaling = axis_scaling
         self._categories_num = categories_num
         self._object_probability = object_probability
         self._nms_threshold = nms_threshold
 
     def process_raw_data(self, infer_results):
         yoloscaled_detections = self._get_detections(infer_results)
-        fullscaled_detections = self._rescale_detections(yoloscaled_detections)
-        filtered_detections = self._nms_filter_detections(fullscaled_detections)
-        screen_clipped_detections = self._clip_to_screen(filtered_detections)
-        return screen_clipped_detections
+        if len(yoloscaled_detections) == 0:
+            return self._generate_empty_detections()
+        else:
+            fullscaled_detections = self._rescale_detections(yoloscaled_detections)
+            filtered_detections = self._nms_filter_detections(fullscaled_detections)
+            screen_clipped_detections = filtered_detections #self._clip_to_screen(filtered_detections)
+            return screen_clipped_detections
 
     def _get_detections(self, infer_results):
-        o = trt_outputs
-        dets = o.reshape((category_num + 4, -1))
+        o = infer_results
+        dets = o.reshape((self._categories_num + OBJECT_COORDINATES_NUM, -1))
         dets = dets.transpose(1, 0)
         scores = dets[:, 4:].max(axis=1)
         classes = dets[:, 4:].argmax(axis=1)
@@ -72,57 +77,49 @@ class YoloPostprocessor:
             np.ones((scores.shape[0], 1))), 
             axis=1)
         # print("Probs ", (probabilities[:, 4] > conf_th).shape)
-        probabilities = probabilities[probabilities[:, 4] > conf_th]
-        # print("Probs2 ", probabilities.shape)
-    detections = probabilities #np.concatenate(probabilities, axis=0)
-    # print(detections.shape)
+        detections = probabilities[probabilities[:, 4] > self._object_probability]
 
-    if len(detections) == 0:
-        boxes = np.zeros((0, 4), dtype=np.int)
-        scores = np.zeros((0,), dtype=np.float32)
-        classes = np.zeros((0,), dtype=np.float32)
-    else:
-        box_scores = detections[:, 4] * detections[:, 6]
+        return detections
 
     def _rescale_detections(self, yoloscaled_detections):
-        # scale x, y, w, h from [0, 1] to pixel values
-        old_h, old_w = img_h, img_w
-        offset_h, offset_w = 0, 0
-        if letter_box:
+        # if letter_box:
+        rescaled_detections = yoloscaled_detections
+        if False:
             if (img_w / input_shape[1]) >= (img_h / input_shape[0]):
                 old_h = int(input_shape[0] * img_w / input_shape[1])
                 offset_h = (old_h - img_h) // 2
             else:
                 old_w = int(input_shape[1] * img_h / input_shape[0])
                 offset_w = (old_w - img_w) // 2
-        detections[:, 0:4] *= np.array(
-            [old_w / input_shape[0], old_h / input_shape[1], old_w / input_shape[0], old_h / input_shape[1]], dtype=np.float32)
-        detections[:, 0] -= detections[:, 2] / 2
-        detections[:, 1] -= detections[:, 3] / 2 
+        rescaled_detections[:, 0:4] *= np.array(
+            [self._axis_scaling[0], self._axis_scaling[1], self._axis_scaling[0], self._axis_scaling[1]], dtype=np.float32)
+        #Yolov8 predicts center of rectangle
+        rescaled_detections[:, 0] -= rescaled_detections[:, 2] / 2
+        rescaled_detections[:, 1] -= rescaled_detections[:, 3] / 2 
+        return rescaled_detections
 
-        # NMS
+    def _nms_filter_detections(self, detections):
+            # NMS
         nms_detections = np.zeros((0, 7), dtype=detections.dtype)
         for class_id in set(detections[:, 5]):
             idxs = np.where(detections[:, 5] == class_id)
             cls_detections = detections[idxs]
-            keep = _nms_boxes(cls_detections, nms_threshold)
+            keep = _nms_boxes(cls_detections, self._nms_threshold)
             nms_detections = np.concatenate(
                 [nms_detections, cls_detections[keep]], axis=0)
 
-    def _nms_filter_detections(self, detections):
         xx = nms_detections[:, 0].reshape(-1, 1)
         yy = nms_detections[:, 1].reshape(-1, 1)
-        if letter_box:
-            xx = xx - offset_w
-            yy = yy - offset_h
+        # if letter_box:
+        #     xx = xx - offset_w
+        #     yy = yy - offset_h
         ww = nms_detections[:, 2].reshape(-1, 1)
         hh = nms_detections[:, 3].reshape(-1, 1)
         boxes = np.concatenate([xx, yy, xx+ww, yy+hh], axis=1)
         boxes = boxes.astype(np.int)
         scores = nms_detections[:, 4] * nms_detections[:, 6]
         classes = nms_detections[:, 5]
-    return boxes, scores, classes
-
+        return boxes, scores, classes
 
     def _clip_to_screen(self, boxes):
         if 1:
