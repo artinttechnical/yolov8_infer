155ddc4a7ae1c24bc7e2aeddc31daedbb78f86ff
Alex Nazarov <artint@ArtintPortableBook>
2023-06-29 20:14:25 +0300

    Yolo Preprocessor works. Output coincides with standard

--- yolo_preprocessor.py
+++ yolo_preprocessor.py
@@ -6,18 +6,18 @@ class LetterBox:
         self._target_img_size = target_img_size
 
     def add_letterbox(self, resized):
-        img_h, img_w, _ = img.shape
-        new_h, new_w = self._target_img_size
+        img_h, img_w, _ = resized.shape
+        new_w, new_h = self._target_img_size
         offset_h, offset_w = 0, 0
         if (new_w / img_w) <= (new_h / img_h):
             ratio = new_w / img_w
             new_h = int(img_h * ratio)
-            offset_h = (self._target_img_size[0] - new_h) // 2
+            offset_h = (self._target_img_size[1] - new_h) // 2
         else:
             ratio = new_h / img_h
             new_w = int(img_w * ratio)
-            offset_w = (self._target_img_size[1] - new_w) // 2
-        img = np.full((self._target_img_size[0], self._target_img_size[1], 3), 127, dtype=np.uint8)
+            offset_w = (self._target_img_size[0] - new_w) // 2
+        img = np.full((self._target_img_size[1], self._target_img_size[0], 3), 127, dtype=np.uint8)
         img[offset_h:(offset_h + new_h), offset_w:(offset_w + new_w), :] = resized
         return img
 
@@ -40,8 +40,8 @@ class OpenCVCapturer:
 
     def read(self):
         _, frame = self._opencv_cap.read()
-        if frame:
-            resized_frame = cv2.resize(frame, (self._resized_size[1], self._resized_size[0]));
+        if 1:
+            resized_frame = cv2.resize(frame, (self._resized_size[0], self._resized_size[1]));
             return (True, (frame, resized_frame))
         else:
             return False, (None, None)
@@ -52,6 +52,6 @@ class OpenCVPreprocessor:
         self._inference_preparer = OpenCVInferencePreparer()
 
     def transform(self, img):
-        img = self._letterboxer(img)
-        img = self._inference_preparer(img)
-        return img
+        img = self._letterboxer.add_letterbox(img)
+        img = self._inference_preparer.prepare_for_infer(img)
+        return np.ascontiguousarray(img)
